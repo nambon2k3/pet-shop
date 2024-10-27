d04a9fa2426cb99a83edd45ad4c1b966d4f589fc
Tu Binh Nguyen <binhntthe176420@fpt.edu.vn>
2024-10-27 17:41:40 +0700

    update: layout manage order

--- app/src/main/java/com/example/petshopapplication/MainActivity.java
+++ app/src/main/java/com/example/petshopapplication/MainActivity.java
@@ -7,13 +7,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.example.petshopapplication.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.payment);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Kiểm tra nếu Activity này được khởi tạo lần đầu tiên, hiển thị Fragment ban đầu
+        if (savedInstanceState == null) {
+            // Đặt Fragment cần hiển thị sau khi đăng nhập
+            replaceFragment(new ProcessingTablayoutFragment());
+        }
+    }
+
+    // Phương thức để thay thế Fragment
+    private void replaceFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
     }
 }
